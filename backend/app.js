9399312e91933634f0dae859423f854fe30c65f2
2dogjack <156984409+2dogjack@users.noreply.github.com>
2024-10-28 17:56:12 -0500

    Update app.js
    changed port listen range

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,6 @@ app.use('/shops', shops);
 app.use('/employees', employeeAuth);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
