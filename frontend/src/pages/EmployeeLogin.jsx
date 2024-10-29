e573d6e21a8eb0b7ce4f6aabb15ba6d5ce5ae92a
2dogjack <156984409+2dogjack@users.noreply.github.com>
2024-10-28 21:21:58 -0500

    Update EmployeeLogin.jsx
    removed '

--- frontend/src/pages/EmployeeLogin.jsx
+++ frontend/src/pages/EmployeeLogin.jsx
@@ -12,7 +12,7 @@ export default function EmployeeLogin() {
   });
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('${import.meta.env.VITE_API_BASE_URL}/employees/auth', values).then((res) => {
+    axios.post(`${import.meta.env.VITE_API_BASE_URL}/employees/auth`, values).then((res) => {
       res.data.Status == "Success"
         ? navigate("/employees")
         : alert(res.data.Error);
