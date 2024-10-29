3a29a765b1cb382baff83ccf805221712fe3cf60
2dogjack <156984409+2dogjack@users.noreply.github.com>
2024-10-28 21:22:59 -0500

    Update Rides.jsx
    removed '

--- frontend/src/pages/Rides.jsx
+++ frontend/src/pages/Rides.jsx
@@ -11,7 +11,7 @@ export default function Rides() {
 
   const ridesList = () => {
     axios
-      .get('${import.meta.env.VITE_API_BASE_URL}/rides/read')
+      .get(`${import.meta.env.VITE_API_BASE_URL}/rides/read`)
       .then((res) => {
         setRides(
           res.data.result.filter((ride) => {
